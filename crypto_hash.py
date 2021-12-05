1e64c6e98f3ff0c82c9c48ab153cdc5fe11c66a9
Grant Robert Thompson <Tower5954@github.com>
2021-12-05 05:16:55 +0000

    commit

--- crypto_hash.py
+++ crypto_hash.py
@@ -2,18 +2,26 @@ import hashlib
 import json
 
 
-def crypto_hash(data):
+def stringify(data):
+    return json.dumps(data)
+
+
+def crypto_hash(*args):
     """
-    Return a sha-256 hash of the given data.
+    Return a sha-256 hash of the given arguments.
     """
 
-    stringified_data = json.dumps(data)
+    stringified_args = map(stringify, args)
 
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+    print(f'stringified_args:{stringified_args}')
+    joined_data = ''.join(stringified_args)
+    print(f'joined_data: {joined_data}')
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 
 def main():
-    print(f"crypto_hash('1'): {crypto_hash(1)}")
+    print(f"crypto_hash('one', 2 ,[3]): {crypto_hash('one',2,[3])}")
 
 
 if __name__ == '__main__':
