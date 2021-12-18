bbb07e845545392fb244b8ed87e16369da23cac1
Grant Robert Thompson <Tower5954@github.com>
2021-12-18 05:40:42 +0000

    commit

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,6 @@
+import time
 from backend.block import Block, GENESIS_DATA
+from backend.config import MINE_RATE, SECONDS
 
 
 def test_mine_block():
@@ -14,7 +16,38 @@ def test_mine_block():
 
 def test_genesis():
     genesis = Block.genesis()
-    
+
     assert isinstance(genesis, Block)
     for key, value in GENESIS_DATA.items():
         getattr(genesis, key) == value
+
+
+def test_quickly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo', )
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty + 1
+
+
+def test_slowly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo', )
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty - 1
+
+
+def test_mined_block_difficulty_limited_at_1():
+    last_block = Block(
+        time.time_ns(),
+        'test_lst_hash',
+        'test_hash',
+        'test_data',
+        1,
+        0
+    )
+
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == 1
